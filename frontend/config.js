15675c4d7ac02985a15039214e0c195d1c3335e6
rohan-khandare <145722302+rohan-khandare@users.noreply.github.com>
2025-04-02 23:29:12 +0530

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL ="https://server-rohan-khandares-projects.vercel.app/api/v1";
+export const BASE_URL ="https://server-git-main-rohan-khandares-projects.vercel.app/api/v1";
 export const token = sessionStorage.getItem("token");
