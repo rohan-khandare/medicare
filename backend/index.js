8eb921b8d12231dad34caa1c45b1cadaf6f2e2e0
rohan-khandare <145722302+rohan-khandare@users.noreply.github.com>
2025-04-02 22:11:01 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -66,7 +66,6 @@
 //     connectDB();
 //     console.log("Server is running on port " + port);
 // });
-
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -79,11 +78,9 @@ import reviewRoute from "./Routes/review.js";
 import bookingRoute from "./Routes/booking.js";
 import paymentRoute from "./Routes/paymentRoute.js";
 import Razorpay from "razorpay";
-import { createServer } from "@vercel/node"; // Required for serverless deployment
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 8000;
 
 const corsOptions = {
     origin: function (origin, callback) {
@@ -91,6 +88,10 @@ const corsOptions = {
         callback(null, true);
     }
 };
+
+app.get('/', (req, res) => {
+    res.send("API is working");
+});
 
 // Database connection
 mongoose.set("strictQuery", false);
@@ -113,20 +114,19 @@ app.use(cors(corsOptions));
 app.use(express.urlencoded({ extended: true }));
 
 // Define routes
-app.use("/api/v1/auth", authRoutes);
-app.use("/api/v1/users", userRoute);
-app.use("/api/v1/doctors", doctorRoute);
-app.use("/api/v1/reviews", reviewRoute);
-app.use("/api/v1/bookings", bookingRoute);
-app.use("/api/v1/payments", paymentRoute);
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/users', userRoute);
+app.use('/api/v1/doctors', doctorRoute);
+app.use('/api/v1/reviews', reviewRoute);
+app.use('/api/v1/bookings', bookingRoute);
+app.use('/api/v1/payments', paymentRoute);
 
-// Razorpay Instance
+// Razorpay instance
 export const instance = new Razorpay({
     key_id: process.env.RAZORPAY_KEY_ID,
     key_secret: process.env.RAZORPAY_KEY_SECRET,
 });
 
-// Serverless export
+// Remove `app.listen()`
 connectDB();
-export default createServer(app);
-
+export default app;
